f03e8ec6c84efe86739f17b9641f42ca0b154154
jm-begon <jm.begon@ulg.ac.be>
2018-09-04 14:10:16 +0200

    Issue sytem - better admin view to add at least one message

--- js_accompany/issues/admin.py
+++ js_accompany/issues/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from .models import *
 
+
+class MessagesForIssues(admin.StackedInline):
+    model = IssueMessage
+    extra = 1
+
+
+class IssueAdmin(admin.ModelAdmin):
+    inlines = [MessagesForIssues]
+
 admin.site.register(IssueState)
-admin.site.register(Issue)
+admin.site.register(Issue, IssueAdmin)
 admin.site.register(IssueMessage)
